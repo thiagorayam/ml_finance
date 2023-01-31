913674db69ea3fcb4fea8ea949b5049573f80fd0
Thiago Rayam <thrsantos@voegol.com.br>
2023-01-31 00:14:40 -0300

    update repo

--- PlotFunctions/plot.py
+++ PlotFunctions/plot.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 
-def plot_label(close,label,figsize=(20,10)):
+def plot_label(close,label,figsize=(20,10),fontsize=20):
+
+    plt.rcParams['xtick.labelsize'] = fontsize
+    plt.rcParams['ytick.labelsize'] = fontsize   
     
     plt.figure(figsize = figsize)
     plt.plot(close,color='k',label='Close')
@@ -8,14 +11,14 @@ def plot_label(close,label,figsize=(20,10)):
     # long position
     long_cond = close.index.isin(label[(label==1)].index)
     plt.plot(close[long_cond].index, close[long_cond], '^',
-                markersize = 15, color='g', label='buy')
+                markersize = fontsize, color='g', label='buy')
 
     # short position
     short_cond = close.index.isin(label[(label==-1)].index)
     plt.plot(close[short_cond].index, close[short_cond], 'v',
-                markersize = 15, color='r', label='sell')
+                markersize = fontsize, color='r', label='sell')
                 
-    plt.xlabel('Date', fontsize = 15 )
-    plt.legend()
+    plt.xlabel('Date', fontsize = fontsize )
+    plt.legend( fontsize = fontsize )
     plt.grid()
     plt.show()
